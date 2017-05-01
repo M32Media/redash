fec39aebe7c2195c5f29e5f9c0a10751d08d5caf
Guillaume <guiloua2@hotmail.com>
2017-05-01 09:49:39 -0400

    Rebranding should be done now

--- client/app/components/footer/index.js
+++ client/app/components/footer/index.js
@@ -1,8 +1,10 @@
 import template from './footer.html';
+import logoUrl from '../../assets/images/m32-logo-small.png';
 
 function controller(clientConfig, currentUser) {
   this.version = clientConfig.version;
   this.newVersionAvailable = clientConfig.newVersionAvailable && currentUser.isAdmin;
+  this.logoUrl = logoUrl;
 }
 
 export default function (ngModule) {
