622cf679bc6d3f249a5be37de2d2e6ce747ab5ed
dev <dev@arnaud-redash-dev-again.c.ad-operations.internal>
2017-05-01 17:18:05 +0000

    Dashgroups and Dashboard sub list

--- client/app/pages/dashgroups/list.js
+++ client/app/pages/dashgroups/list.js
@@ -6,23 +6,38 @@ import template from './list.html';
 function DashgroupsCtrl($scope, $location, $q, currentUser, Events, Dashgroup) {
   Events.record('view', 'page', 'admin/dashgroups');
 
+  $scope.detailedDashgroups = [];
+
   const promises = {
-    groups: Dashgroup.groups(),
-    dashgroupDashboard: Dashgroup.dashgroupDashboard()
+    groups: Dashgroup.groups().$promise,
+    dashgroupDashboard: Dashgroup.dashgroupDashboard().$promise
   }
 
+  //To call multiple promises
   $q.all(promises).then((results) => {
-    
-    _.each(results, function(result){
-      console.log(result)
-    })
 
+    //For each dashgroup find dashboards
+    _.each(results.groups, (group) => {
 
+      var dashboards = [];
+
+      //Find all dashboards inside dashgroup
+      dashboards = _.filter(results.dashgroupDashboard, (dgdb) => {
+
+        return group.id == dgdb.dashgroup_id
+          
+      });
+
+      //Add 
+      group.dashboards = dashboards;
+
+      console.log(group.dashboards)
+
+      $scope.detailedDashgroups.push(group);
+
+    });
 
   });
-
-  
-
 
 }
 
