0491c0d1b3a9ad4a36898cbc70c85f005074ad38
Guillaume <guiloua2@hotmail.com>
2017-05-09 13:26:02 -0400

    Removes filters from the widgets themselves

--- client/app/visualizations/index.js
+++ client/app/visualizations/index.js
@@ -97,7 +97,7 @@ function VisualizationRenderer(Visualization) {
     // TODO: using switch here (and in the options editor) might introduce errors and bad
     // performance wise. It's better to eventually show the correct template based on the
     // visualization type and not make the browser render all of them.
-    template: `<filters filters="filters"></filters>\n${Visualization.renderVisualizationsTemplate}`,
+    template: `${Visualization.renderVisualizationsTemplate}`,
     replace: false,
     link(scope) {
       scope.$watch('queryResult && queryResult.getFilters()', (filters) => {
