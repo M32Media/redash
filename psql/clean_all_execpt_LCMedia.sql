f41d4ec87c9c1fd659a9b3d87e1050e9927aae71
Edoardo Dewhurst <40395157+edoardodewhurst@users.noreply.github.com>
2020-12-17 19:34:17 -0500

    Delete clean_all_execpt_LCMedia.sql

--- psql/clean_all_execpt_LCMedia.sql
+++
@@ -1,4 +0,0 @@
-delete from widgets where visualization_id in (select id from visualizations where query_id in (select id from queries  where query not like '%LCM%'));
-delete from visualizations where query_id in (select id from queries  where query not like '%LCM%');
-delete from queries  where query not like '%LCM%';
-delete from dashboards where id in (select id from dashboards where name not like '%LCM%');
